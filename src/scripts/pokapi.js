db41321bee71cae9394283f9697f695e781c448e
Tony Blard <tonyblard55@gmail.com>
2023-05-06 13:58:17 +0200

    [ADD] PokemonSpecies typedef for getPokemonsList() method
    Return type for getPokemonsList() and getPokemonCount() methods

--- src/scripts/pokapi.js
+++ src/scripts/pokapi.js
@@ -1,14 +1,33 @@
+/**
+ * @typedef PokemonSpecies
+ * @type {object}
+ * @property {number} count
+ * @property {?string} next
+ * @property {?string} previous
+ * @property {{name: string, url: string}[]} results
+ *
+ * @param {number} limit
+ * @param {number} offset
+ * @returns {Promise<PokemonSpecies.results>}
+ */
 export async function getPokemonsList(limit, offset) {
     const url = "https://pokeapi.co/api/v2/pokemon-species?limit=" + limit + "&offset=" + offset;
     const response = await fetch(url);
-    const data = await response.json();
 
+    /** @type {PokemonSpecies} */
+    const data = await response.json();
     return data.results;
 }
 
+/**
+ *
+ * @returns {Promise<number>}
+ */
 export async function getPokemonCount() {
     const url = "https://pokeapi.co/api/v2/pokemon-species?limit=100000";
     const response = await fetch(url);
+
+    /** @type {PokemonSpecies} */
     const data = await response.json();
 
     return data.count;
