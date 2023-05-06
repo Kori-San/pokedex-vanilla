338191b5d79b7cc16318222b3b156f8947951fef
Tony Blard <tonyblard55@gmail.com>
2023-05-06 17:51:05 +0200

    [ADD] Use hideLoader method when windows load Add setTimeout to display it 3s

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,12 @@
 import { buildNextPrevButtons, buildPokemonContainer } from "/lib/pokedex.js";
+import {hideLoader} from '/lib/utilities.js';
 
+const pokeballLoader = document.querySelector('.container-loader');
 /* Loads and displays all Pokemons in a range made with Offset and Limit vars local to 'pokedex.js'. */
 window.addEventListener('load', async () => {
     buildNextPrevButtons();
     await buildPokemonContainer();
-    console.log("load");
+    setTimeout(() => {
+        hideLoader(pokeballLoader);
+    }, 3000)
 })
