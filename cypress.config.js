3346e93de2f564698aaa10336ee7dc6096f62c97
aezzayer <aezzayer@selceon.com>
2023-05-30 21:50:37 +0200

    [ADD] e2e test + gremlins (monkey testing)

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:80',
+    baseUrl: 'https://pokedex-qe1p1dckh-kori-san.vercel.app',
   },
 });
--- cypress/e2e/e2e.cy.js
+++ cypress/e2e/e2e.cy.js
@@ -1,3 +1,5 @@
+import { createHorde } from 'gremlins.js';
+
 describe('visit', () => {
   beforeEach(() => {
     cy.visit('/');
@@ -7,10 +9,40 @@ describe('visit', () => {
     cy.get(':nth-child(1) > .pokemon-name').should('contain', 'Bulbasaur');
   });
 
-
   it('check Bulbasaur page', () => {
     cy.get(':nth-child(1) > .pokemon-name').click();
     cy.get('#pokemon-name').should('contain', 'Bulbasaur');
   });
 
+  it('check search bar', () => {
+    cy.get('#search-bar').type('pikachu');
+    cy.get('.pokemon-name').contains('Pikachu');
+  });
+
+  it('check next button', () => {
+    cy.get('#next-button').click();
+    cy.get('#pokedex-interval').contains('36 - 70');
+  });
+
+  it('check page limit equal 15', () => {
+    cy.get('#limit-input').clear().type('15');
+    cy.get(':nth-child(15) > .pokemon-name').contains('Beedrill');
+  });
+
 })
+
+describe('Run gremlins.js inside a cypress test', () => {
+  let horde;
+  beforeEach(() => {
+    cy.visit('/')
+    cy.window().then((testedWindow) => {
+      horde = createHorde({ window: testedWindow });
+    })
+  });
+  it('should run gremlins.js', () => {
+    horde.unleash()
+    setTimeout(() => {
+      horde.stop()
+    }, 10000)
+  });
+});
