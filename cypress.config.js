49359c6e7b120e15876c797ed723336a11d742aa
Aymen EZZAYER <aezzayer@selceon.com>
2023-06-26 11:58:48 +0200

    [ADD] Cypress CI and new Alpine image

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
     e2e: {
-        baseUrl: 'https://pokedex-qe1p1dckh-kori-san.vercel.app',
+        baseUrl: 'http://localhost',
     },
 });
